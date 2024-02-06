0cf190d249842159580b52fa7efea964290219f4
MassiGy <ghernaoutmassi@gmail.com>
2024-02-06 17:10:55 +0100

    +master: added a test generator for HilberMatrixes of dim going from 3 to 15

--- src/main/java/AlgLin/HilbertMatrice.java
+++ src/main/java/AlgLin/HilbertMatrice.java
@@ -28,6 +28,7 @@ public class HilbertMatrice extends Matrice {
 
     public static void main(String[] args) {
 
+        /*
         Scanner sc = new Scanner(System.in);
         String res = "";
         while (res.length() < 1 || !estEntierPositif(res)){
@@ -74,6 +75,52 @@ public class HilbertMatrice extends Matrice {
 
         }catch(Exception e){
             e.printStackTrace();
+        }
+        */
+
+
+        for (int i = 3; i <= 15; i++) {
+
+            System.out.printf("\n################# Ordre = %d ##############\n",i);
+
+            int ordre = i;
+            HilbertMatrice hm = new HilbertMatrice(ordre);
+            HilbertMatrice hmCopie = new HilbertMatrice(ordre);
+
+
+            try{
+
+                // calc de l'inverse de hm
+                Matrice inverseHm = hm.inverse();
+
+                System.out.println("Inverse de la matrice de Hilbert d'ordre " + ordre + ": \n" + inverseHm);
+                System.out.println("Son conditionnement avec L1: " + hmCopie.calcConditionnement((m) -> Matrice.L1_norme(m)));
+                System.out.println("Son conditionnement avec Linf: " + hmCopie.calcConditionnement((m) -> Matrice.Linf_norme(m)));
+
+                // calc du produit entre la matrice (copie) et son inverse
+                Matrice produitHmEtSonInverse = Matrice.produit(hmCopie, inverseHm);
+
+                Matrice resultatSoustractionId = Matrice.soustraction(produitHmEtSonInverse, Matrice.getIdentite(ordre));
+
+                double L1_norme = resultatSoustractionId.L1_norme();
+                double Linf_norme = resultatSoustractionId.Linf_norme();
+
+
+                if (L1_norme < Matrice.numerical_epsilon) {
+                    System.out.println("Test réussi, norme_1(AA^-1 - I) est suffisamment petite: " + L1_norme);
+                } else {
+                    System.out.println("Test échoué, norme_1(AA^-1 - I) est trop grande: " + L1_norme);
+                }
+
+                if (Linf_norme < Matrice.numerical_epsilon) {
+                    System.out.println("Test réussi, norme_inf(AA^-1 - I) est suffisamment petite: " + Linf_norme);
+                } else {
+                    System.out.println("Test échoué, norme_inf(AA^-1 - I) est trop grande: " + Linf_norme);
+                }
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
